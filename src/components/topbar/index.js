e239ee9a33bc71fa1e5a0a2727f2b8d1654f241a
Sanin Adnan <sanin17@gmail.com>
2019-05-17 15:54:10 -0400

    Setting up initial menu bar and adding docker file

---
+++ src/components/topbar/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import withRoot from '../../withRoot';
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+  };
+
+class TopBar extends React.Component {
+    state = {
+        open: false,
+      };
+    
+      handleClose = () => {
+        this.setState({
+          open: false,
+        });
+      };
+    
+      handleClick = () => {
+        this.setState({
+          open: true,
+        });
+      };
+    render() { 
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" className={classes.grow}>
+                    Cup of Sugar
+                </Typography>
+                <Button color="inherit">Sign Up</Button>
+                <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            </div>
+        );
+    }
+}
+
+TopBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRoot(withStyles(styles)(TopBar));
